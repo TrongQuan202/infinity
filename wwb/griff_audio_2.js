19e66658b3f5edd4c5e82405452920e14b89ca3c
Doan Trong Quan <trongquan202@gmail.com>
2025-01-16 15:22:35 +0700

    rm

--- wwb/griff_audio_2.js
+++
@@ -1,8 +0,0 @@
-document.write('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="493" height="140" id="griff_audio_2" align="middle">');
-document.write('<param name="allowScriptAccess" value="sameDomain" />');
-document.write('<param name="movie" value="http://www.worldwidebrands.com/wwb/griff_player.swf" />');
-document.write('<param name="loop" value="false" />');
-document.write('<param name="quality" value="high" />');
-document.write('<param name="bgcolor" value="#ffffff" />');
-document.write('<embed loop="false" src="http://www.worldwidebrands.com/wwb/griff_player.swf" quality="high" bgcolor="#ffffff" width="493" height="140" name="griff_audio_2" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');
-document.write('</object>');
